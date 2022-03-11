e8a9edec6ae1d7aaf2d6eab573ff170a10f62b3a
PaulBallestra <paulballestra@yahoo.fr>
2022-03-11 11:10:41 +0100

    Added navigation container

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import {AppRegistry} from 'react-native';
-import App from './App';
+
+//import App from './App';
+
+import rootContainer from './src/navigation';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => rootContainer);
---
+++ src/navigation/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import { NavigationContainer } from '@react-navigation/native'
+//import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import {View, Text} from 'react-native';
+
+//const Stack = createNativeStackNavigator();
+
+const rootContainer = () => {
+  return (
+    <View>
+      <Text> Cajoo </Text>
+    </View>
+  );
+};
+
+export default rootContainer;
